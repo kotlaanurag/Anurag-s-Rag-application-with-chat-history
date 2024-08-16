5dea292d7180cf90d0ecf5ebc41a7b201059fc1e
kotlaanurag <anuragvenkatareddy22@gmail.com>
2024-08-16 14:00:48 -0500

    Anurags Rag application

---
+++ app.py
@@ -0,0 +1,113 @@
+import streamlit as st
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.vectorstores import Chroma
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_groq import ChatGroq
+from langchain.embeddings import HuggingFaceEmbeddings
+import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer
+from langchain_community.embeddings import GPT4AllEmbeddings
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Importing from .env file
+os.environ["HF_TOKEN"] = os.getenv("HF_TOKEN")
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+#embeddings = SentenceTransformer("all-MiniLM-L6-v2")
+
+# Setup Streamlit
+st.title("Anurag's RAG with PDF uploads and chat history")
+st.write("Upload PDFs and chat with their content")
+
+# Import the Groq API Key
+api_key = st.text_input("Enter your Groq API Key", type="password")
+
+# Check if Groq API key is provided
+if api_key:
+    llm = ChatGroq(groq_api_key=api_key, model_name="Gemma2-9b-It")
+
+    # Statefully manage chat history
+    session_id = st.text_input("Session ID", value="default_session")
+
+    if 'store' not in st.session_state:
+        st.session_state.store = {}
+
+    uploaded_files = st.file_uploader("Choose a PDF file", type="pdf", accept_multiple_files=True)
+
+    if uploaded_files:
+        documents = []
+        for uploaded_file in uploaded_files:
+            temppdf = "./temp.pdf"
+            with open(temppdf, "wb") as file:
+                file.write(uploaded_file.getvalue())
+            loader = PyPDFLoader(temppdf)
+            docs = loader.load()
+            documents.extend(docs)
+        
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=500)
+        splits = text_splitter.split_documents(documents)
+        vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+        retriever = vectorstore.as_retriever()
+
+        contextualize_q_system_prompt = (
+            """Given a chat history and the latest user question which might reference context in the chat
+            history, formulate a standalone question which can be understood without the chat history. Do not answer the question,
+            just reformulate it if needed and otherwise return it as is."""
+        )
+        contextualize_q_prompt = ChatPromptTemplate.from_messages(
+            [("system", contextualize_q_system_prompt),
+             MessagesPlaceholder("chat_history"),
+             ("human", "{input}")]
+        )
+        history_aware_retriever = create_history_aware_retriever(llm, retriever, contextualize_q_prompt)
+
+        system_prompt = (
+            """You are an assistant for question-answering tasks. Use the following pieces of retrieved context
+            to answer the question. If you don't know the answer, say that you don't know. Use three sentences maximum and keep the answer concise."""
+            "\n\n"
+            "{context}"
+        )
+        qa_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system_prompt),
+                MessagesPlaceholder("chat_history"),
+                ("human", "{input}")
+            ]
+        )
+        question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+        rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+        def get_session_history(session: str) -> BaseChatMessageHistory:
+            if session not in st.session_state.store:
+                st.session_state.store[session] = ChatMessageHistory()
+            return st.session_state.store[session]
+
+        conversational_rag_chain = RunnableWithMessageHistory(
+            rag_chain,
+            get_session_history,
+            input_messages_key="input",
+            history_messages_key="chat_history",
+            output_messages_key="answer"
+        )
+
+        user_input = st.text_input("Your question")
+
+        if user_input:
+            session_history = get_session_history(session_id)
+            response = conversational_rag_chain.invoke(
+                {"input": user_input},
+                config={"configurable": {"session_id": session_id}}
+            )
+            st.write("Assistant:", response["answer"])
+            st.write("Chat history:", session_history.messages)
+
+else:
+    st.warning("Please enter the Groq API Key.")
---
+++ readme.md
@@ -0,0 +1,12 @@
+***->if you are facing issues to install the touch liberary you can use these commands
+# Conda
+conda install pytorch torchvision cpuonly -c pytorch
+# Pip
+pip install torch==1.5.0+cpu torchvision==0.6.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+
+
+***-> if you face any issue with chroma db installation then
+You need to install the required build tools. You can download and install them from the following link:
+
+Microsoft Visual C++ Build Tools:link:(https://visualstudio.microsoft.com/visual-cpp-build-tools/)
+During installation, ensure that you include the "Desktop development with C++" workload. This will provide the necessary compilers and libraries needed to build the package.
